675cc9bdf99e5dfaf6a1555c5e8f3be26e381f7c
Ali Zedan <ali2016zedan@gmail.com>
2019-11-01 20:41:06 +0100

    har gjort oppgave 5,6

--- Oblig3/src/no/oslomet/cs/algdat/Oblig3/mAIN.java
+++ Oblig3/src/no/oslomet/cs/algdat/Oblig3/mAIN.java
@@ -39,13 +39,14 @@ public class mAIN {
         System.out.println ("-------------");
 */
       //  Oppgave3
+        /*
         int[] i = {4,7,2,9,4,10,8,7,6,4,1};
         ObligSBinTre<Integer> tree = new ObligSBinTre<>(Comparator.naturalOrder());
         for (int verdi : i) tree.leggInn(verdi);
         System.out.println(tree);
 
 
-
+/*
         int [] a = {4,7,2,9,4,10,8,7,4,6,1};
         ObligSBinTre<Integer> tre = new ObligSBinTre<>(Comparator. naturalOrder ());
         for ( int verdi : a) tre.leggInn(verdi);
@@ -53,10 +54,15 @@ public class mAIN {
         tre.fjernAlle(7); tre.fjern(8);
         System. out .println(tre.antall()); // 5
         System. out .println(tre + " " + tre.toString());
-
+*/
 // [1, 2, 6, 9, 10] [10, 9, 6, 2, 1]
 // OBS: Hvis du ikke har gjort oppgave 4 kan du her bruke toString()
 
+        //oppgave 6
+        ObligSBinTre<Character> tre = new ObligSBinTre<>(Comparator. naturalOrder ());
+        char [] verdier = "IATBHJCRSOFELKGDMPQN" .toCharArray();
+        for ( char c : verdier) tre.leggInn(c);
+        System. out .println(tre.høyreGren() + " " + tre.lengstGren());
 
 
     }
